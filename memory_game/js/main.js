3110efe3a27c404f78fcd7ad57292cb088ab5079
System Administrator <korpx94@gmail.com>
2019-03-07 18:02:51 -0500

    added js file. skipping some steps here

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log('Up and running!')
+
+var cardOne = 'Queen';
+var cardTwo = 'king';
+var cardThree = 'Queen';
+var cardFour = 'King';
+
+console.log('User flipped ' + cardTwo)
+console.log('User flipped ' + cardFour);
