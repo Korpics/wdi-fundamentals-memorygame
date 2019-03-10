9b3759e790077be869e7fda50f857500e8df8145
System Administrator <korpx94@gmail.com>
2019-03-10 16:30:51 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,47 @@
 console.log('Up and running!')
+console.log('please select a card \'0-3\' by using flipCard');
 
-var cards = [ 'queen', 'queen', 'king', 'king'];
+var cards = [
+{
+rank: "queen",	
+suit: "hearts",
+cardImage:"images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage:	"images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage:"images/king-of-hearts.png" 
+},
+{
+rank: "king",
+suit:"diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
+  alert("You found a match!");
 } else {
-  console.log("Sorry, try again.");
+  alert("Sorry, try again.");
 }
 }
 
 var flipCard = function(cardID) {
-	console.log('User flipped ' + cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+	console.log('User flipped ' + cards[cardID].rank);
+	cardsInPlay.push(cards[cardID].name);
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
 if (cardsInPlay.length === 2){
 	checkForMatch();
 } else console.log('pick another card')
-}
+};
 /*console.log(cardsInPlay);
 console.log(flipCard(0));*/
 
